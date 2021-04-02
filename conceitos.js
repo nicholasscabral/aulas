31731240d8600db06245050a75743faae0dd743a
nicholasscabral <nicholasscabral@gmail.com>
2021-04-01 21:51:53 -0300

    conceitos js

---
+++ conceitos.js
@@ -0,0 +1,60 @@
+// variaveis
+
+// escopo é o local que "prende" a variavel
+
+/*
+  const == global e local
+  var == global e local
+  let == global local e em blocos
+
+  global : pode ser acessado no codigo inteiro, eh definido GLOBALMENTE
+  local: so pode ser acessado dentro de funções
+  em blocos: so pode ser acessado dentro de blocos (if, else, for, while, abre e fecha com {})
+*/
+
+// condicionais
+// const aluno = "Jorge";
+// var nota = prompt("digite a nota");
+
+// if (nota >= 7 && nota <= 10) {
+//   console.log(aluno + " passou na prova com a nota " + nota);
+// } else {
+//   console.log(aluno + " NÃO passou na prova, pois tirou " + nota);
+// }
+
+// for
+// for (i = 1; i <= 10; i++) {
+//   let index = i;
+//   console.log(index);
+// }
+
+// while
+// var indice = 0;
+
+// while (indice < 10) {
+//   console.log(indice);
+//   indice++;
+// }
+
+// function boasVindas() {
+//   var nome = prompt("digite seu nome");
+
+//   alert("seja bem vindo " + nome);
+// }
+
+// function soma(a, b) {
+//   var soma = a + b;
+//   return soma;
+// }
+
+// var minhaFuncao = function () {};
+
+// // arrow function
+// var ArrowFunction = () => {
+//   console.log(" isso eh uma arrow function");
+// };
+
+// // função anonima ( como parematro)
+// (parametros) => {
+//   console.log("aqui é a execução da função anonima");
+// };
